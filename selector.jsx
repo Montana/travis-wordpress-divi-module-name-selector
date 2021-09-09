44fbe04b92f8d03dc2bd6d90716bbfc083acaa30
Montana Mendy <montana@linux.com>
2021-09-09 13:31:55 -0700

    module search

--- selector.jsx
+++ selector.jsx
@@ -4,7 +4,6 @@ import './style.css';
 
 // Internal dependencies
 
-
 class Prefix_Module_Name extends Component {
   static slug = "prefix_module_name"; // Travis slug can be called by $SLUG -> Montana for example has this laid out in PostQuery. 
 

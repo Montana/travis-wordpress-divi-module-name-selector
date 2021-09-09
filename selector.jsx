7ba89a9207b9c7b6c1012c52bbd57cc5fca1284f
Montana Mendy <montana@linux.com>
2021-09-09 13:36:47 -0700

    postquery

--- selector.jsx
+++ selector.jsx
@@ -5,7 +5,7 @@ import './style.css';
 // Internal dependencies
 
 class Prefix_Module_Name extends Component {
-  static slug = "prefix_module_name"; // Travis slug can be called by $SLUG -> Montana for example has this laid out in PostQuery. 
+  static slug = "prefix_module_name"; // Travis slug can be called by $SLUG -> For example I have this laid out in PostQuery. 
 
   static css(props) {
     const additionalCss = [];
